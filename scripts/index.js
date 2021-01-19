75876c16d7ba124b2a8c96cb0178d31ad06994d8
Ahuoyiza <sophieoyiza@gmail.com>
2021-01-19 23:45:43 +0100

    mobile nav

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+function toggle() {
+    let navbar = document.getElementById("navMobile");
+    if (navbar.style.display === "flex") {
+      navbar.style.display = "none";
+    } else {
+      navbar.style.display = "flex";
+    }
+  }
